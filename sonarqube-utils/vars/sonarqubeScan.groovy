7f7236308f0a05439df0e2353a6c7c6de1ab90f2
cloudsheger <cloudsheger@gmail.com>
2023-10-30 15:49:59 -0400

    test

--- sonarqube-utils/vars/sonarqubeScan.groovy
+++
@@ -1,5 +0,0 @@
-def call(String projectKey, String projectName, String sonarHost, String sonarToken) {
-    def sonarScanOutput = new GroovyShell().parse(new File("${JENKINS_HOME}/workspace/your-job-name/src/sonarscan.py")).sonar_scan(projectKey, projectName, sonarHost, sonarToken)
-    
-    echo "SonarQube Scan Result:\n${sonarScanOutput}"
-}
